17ad576f4a84b82b8fe9edbed44c418ad83b2a21
MoisesCDAW <moisescamposdaw@gmail.com>
2024-12-07 15:49:37 +0000

    Comienzo de implementación del carrito de compras

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
  * API que usa el programa.
  */
 const cocteleriaAPI = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Rum";
+let datos; // datos obtenidos de la API
 
 
 /**
@@ -59,13 +60,100 @@ async function obtenerDatos(API) {
             }
         })
 
-        .then(datos => {
-            constructor_tarjetas(datos);
+        .then(datosAPI => {
+            datos = datosAPI;
+            document.querySelector("#carrito").addEventListener("click", constructor_carrito);
+            constructor_tarjetas();
         })
 
         .catch(err => {
             console.error("ERROR: ", err.message)
         });
+}
+
+
+function masCantidad(boton){
+
+}
+
+
+function menosCantidad(boton){
+    let carrito = localStorage.getItem("carrito");
+
+    if (carrito) {
+        carrito = JSON.parse(carrito);
+    }
+
+    localStorage.setItem("carrito", carrito);
+}
+
+
+function agregarProducto(producto){
+    let carrito = localStorage.getItem("carrito");
+    let existe = false;
+
+    if (!carrito) {
+        // carrito = {drinks: [{id: producto.target.id, nombre: , cantidad: 1}]};
+    }else {
+        carrito = JSON.parse(carrito);
+
+        // let bebidas = new Map(carrito.map(bebida=>[bebida.id, bebida]));
+
+        if (existe) {
+            
+        }else {
+            carrito.drinks.push({nombre: producto.target.id, cantidad: 1});
+        }
+        
+    }
+
+    alert("Tu cóctel '" + producto.target.id  + "' fue agregado al carrito!");
+    localStorage.setItem("carrito", JSON.stringify(carrito));
+}
+
+
+/**
+ * Gestiona la construcción y visualización del carrito de comprar.
+ * La función crea dinámicamente un bloque con cada bebida que esté en el carrito y la agrega al contenedor correspondiente en el DOM.
+ * El carrito de obtiene de un objeto tipo JSON alojado como String en LocalStorage "carrito".
+ */
+function constructor_carrito(){
+    let carrito = localStorage.getItem("carrito");
+
+    if (carrito) {
+        carrito = JSON.parse(carrito);
+        carrito = carrito.drinks;
+        
+        if (document.querySelector(".productos")) {
+            document.querySelector(".productos").remove();
+        }
+        const contenedor = crearElemento("div", {}, ["productos", "offcanvas-body", "d-flex",  "flex-column", "gap-2"]);
+        document.querySelector(".content-carrito").append(contenedor);
+        
+
+        carrito.forEach((bebida)=>{
+            const contentProducto = crearElemento("div", {}, ["d-flex", "justify-content-between", "align-items-center"]);
+            const nombre = crearElemento("p", {}, ["m-0"]);
+            const cantidad = crearElemento("p", {id:"cantidad"}, ["m-0", "d-flex", "align-items-center", "me-4"]);
+            const contentBotones = crearElemento("div", {}, ["d-flex", "gap-2"]);
+            const botonMas = crearElemento("button", {}, ["btn"]);
+            const botonMenos = crearElemento("button", {}, ["btn"]);
+            const imgMas = crearElemento("img", {src:"img/mas.png", width:"14px"});
+            const imgMenos = crearElemento("img", {src:"img/menos.png", width:"14px"});
+            
+            contenedor.append(contentProducto);
+            contentProducto.append(nombre, contentBotones);
+            nombre.append(bebida.nombre);
+            cantidad.append(bebida.cantidad);
+            contentBotones.append(cantidad, botonMas, botonMenos);
+            botonMas.append(imgMas);
+            botonMenos.append(imgMenos);
+
+            botonMas.addEventListener("click", masCantidad);
+            botonMenos.addEventListener("click", menosCantidad);
+        
+        });
+    }
 }
 
 
@@ -75,7 +163,7 @@ async function obtenerDatos(API) {
  * 
  * @param {Object} datos - Los datos obtenidos de la API. Se espera que el objeto contenga una propiedad `drinks` que es un array de bebidas.
  */
-function constructor_tarjetas(datos) {
+function constructor_tarjetas() {
     const bebidas = datos.drinks;
     const contenedor = document.querySelector(".fila-bebidas");
     const descrip = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae similique animi enim";
@@ -86,8 +174,8 @@ function constructor_tarjetas(datos) {
         const contentBody = crearElemento("div", {}, ["card-body", "d-flex", "flex-column", "text-white",]);
         const nombre = crearElemento("h5", {}, ["card-title"]);
         const desc = crearElemento("p", {}, ["card-text"]);
-        const addCarrito = crearElemento("button", {value:bebida.strDrink}, ["btn", "text-white", "rounded-pill"]);
-        const imgCarrito = crearElemento("img", {src:"img/carrito.png", width:"22px"});
+        const addCarrito = crearElemento("button", {id:bebida.idDrink}, ["btn", "text-white", "rounded-pill"]);
+        const imgCarrito = crearElemento("img", {src:"img/carrito.png", width:"22px", id:bebida.strDrink});
 
         contenedor.append(contentTarj);
         contentTarj.append(img, contentBody);
@@ -96,14 +184,13 @@ function constructor_tarjetas(datos) {
         nombre.append(bebida.strDrink);
         desc.append(descrip);
         addCarrito.append(imgCarrito);
+
+        addCarrito.addEventListener("click", agregarProducto);
     });
 }
-
-
-
-
 
 // ================= INICIO ==================
 obtenerDatos(cocteleriaAPI);
 
-
+// localStorage.setItem("carrito", '{"drinks": [{"nombre": "bebida-uno", "cantidad": 2}, {"nombre": "bebida-dos", "cantidad": 1}]}');
+// localStorage.removeItem("carrito");
